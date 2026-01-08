93a5f3de007d0007f5af75d6ac24f84573fa7bc1
25335a0501 <25335a0501@mvgrce.edu.in>
2026-01-08 22:48:10 +0530

    Add files via upload

---
+++ Week3/Experiment1.py
@@ -0,0 +1,11 @@
+#1D Array Operations Using Numpy Module
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("Array:",arr)
+print("Element at index 2:",arr[2])
+arr[2]=60
+print("Element after changing at index 2:",arr[2])
+print("Sliced Array(1 to 4):",arr[1:4])
+fancy_items=[0,2]
+fancy_elements=arr[fancy_items]
+print("Fancy Indexing[0,2]:",fancy_elements)
